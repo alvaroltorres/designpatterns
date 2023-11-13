bda57c4e5ca11004708c7efa4f34d6e71c58031b
unknown <up202208954@up.pt>
2023-11-13 18:17:39 +0000

    still in Step 3.

--- src/main/java/Bar.java
+++ src/main/java/Bar.java
@@ -1,6 +1,22 @@
+import java.util.List;
+
 public abstract class Bar {
     abstract public boolean isHappyHour();
     abstract public void startHappyHour();
     abstract public void endHappyHour();
+
+    List<BarObserver> observers;
+
+    public void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+    public void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
+    }
 
 }
